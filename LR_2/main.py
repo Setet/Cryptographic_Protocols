0f42de5bcd0e38a427e400a35e675fb936005f5f
Artem Yakhontov <72457086+Setet@users.noreply.github.com>
2023-02-28 10:47:12 +0300

    v1.01

--- LR_2/main.py
+++ LR_2/main.py
@@ -1,5 +1,4 @@
-# function that implements Extended euclidean
-# algorithm
+# расширенный евклид
 def extended_euclidean(a, b):
     if a == 0:
         return b, 0, 1
@@ -8,63 +7,51 @@ def extended_euclidean(a, b):
         return g, x - (b // a) * y, y
 
 
-# modular inverse driver function
+# модульная обратная функция
 def modinv(a, m):
     g, x, y = extended_euclidean(a, m)
     return x % m
 
 
-# function implementing Chinese remainder theorem
-# list m contains all the modulii
-# list x contains the remainders of the equations
+# функция кит теоремы
 def crt(m, x):
-    # We run this loop while the list of
-    # remainders has length greater than 1
     while True:
 
-        # temp1 will contain the new value
-        # of A. which is calculated according
-        # to the equation m1' * m1 * x0 + m0'
-        # * m0 * x1
         temp1 = modinv(m[1], m[0]) * x[0] * m[1] + \
                 modinv(m[0], m[1]) * x[1] * m[0]
 
-        # temp2 contains the value of the modulus
-        # in the new equation, which will be the
-        # product of the modulii of the two
-        # equations that we are combining
         temp2 = m[0] * m[1]
+        print("Temp 1: " + str(temp1))
+        print("Temp 2: " + str(temp1))
 
-        # we then remove the first two elements
-        # from the list of remainders, and replace
-        # it with the remainder value, which will
-        # be temp1 % temp2
+        # это я удаляю первые два элемента
+        # из списка остатков и заменяю
+        # с остатком, который будет
+        # быть temp1 % temp2
         x.remove(x[0])
         x.remove(x[0])
         x = [temp1 % temp2] + x
 
-        # we then remove the first two values from
-        # the list of modulii as we no longer require
-        # them and simply replace them with the new
-        # modulii that  we calculated
+        # затем я удаляю первые два значения из
+        # списка модулей, так как они мне больше не нужны
+        # и их просто заменил новыми
+        # модулями, которые я рассчитал
         m.remove(m[0])
         m.remove(m[0])
         m = [temp2] + m
 
-        # once the list has only one element left,
-        # we can break as it will only  contain
-        # the value of our final remainder
         if len(x) == 1:
             break
 
-    # returns the remainder of the final equation
     return x[0]
 
 
 def main():
-    # driver segment
-    m = [3, 4, 7]
+    # список m содержит все модули
+    # list x содержит остатки уравнений
+    # Пример my=x(mod m)
     x = [1, 1, 0]
+    m = [3, 4, 7]
     print(crt(m, x))
 
 
